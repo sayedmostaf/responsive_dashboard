adf458b0a08a2187d5ada4d80552144a55742925
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-09-27 04:08:36 +0300

    user info list tile part two

--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -11,15 +11,19 @@ class UserInfoListTile extends StatelessWidget {
   final String image, title, subtitle;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(image),
-      title: Text(
-        title,
-        style: AppStyles.styleSemiBold16,
-      ),
-      subtitle: Text(
-        subtitle,
-        style: AppStyles.styleRegular12,
+    return Card(
+      color: const Color(0xFFFAFAFA),
+      elevation: 0,
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: AppStyles.styleSemiBold16,
+        ),
+        subtitle: Text(
+          subtitle,
+          style: AppStyles.styleRegular12,
+        ),
       ),
     );
   }
