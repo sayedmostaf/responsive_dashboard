db456dc2f7636565b4ab76c61a3c67961a5a37ea
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-04 18:43:28 +0300

    custom text field

--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/custom_background_container.dart';
+import 'package:responsive_dashboard/widgets/custom_text_field.dart';
 import 'package:responsive_dashboard/widgets/latest_transaction.dart';
 import 'package:responsive_dashboard/widgets/quick_invoice_header.dart';
 
@@ -13,6 +14,10 @@ class QuickInvoice extends StatelessWidget {
       children: [
         QuickInvoiceHeader(),
         LatestTransaction(),
+        Divider(
+          height: 48,
+        ),
+        CustomTextField(),
       ],
     ));
   }
