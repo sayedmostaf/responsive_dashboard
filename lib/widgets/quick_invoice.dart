2ab871cd7770a9793573352dfc92b99ac57832d3
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-04 18:53:21 +0300

    title text field

--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/custom_background_container.dart';
-import 'package:responsive_dashboard/widgets/custom_text_field.dart';
 import 'package:responsive_dashboard/widgets/latest_transaction.dart';
 import 'package:responsive_dashboard/widgets/quick_invoice_header.dart';
+import 'package:responsive_dashboard/widgets/title_text_field.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
@@ -10,15 +10,19 @@ class QuickInvoice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomBackgroundContainer(
-        child: Column(
-      children: [
-        QuickInvoiceHeader(),
-        LatestTransaction(),
-        Divider(
-          height: 48,
-        ),
-        CustomTextField(),
-      ],
-    ));
+      child: Column(
+        children: [
+          QuickInvoiceHeader(),
+          LatestTransaction(),
+          Divider(
+            height: 48,
+          ),
+          TitleTextField(
+            title: 'Customer name',
+            hint: 'Type customer name',
+          ),
+        ],
+      ),
+    );
   }
 }
