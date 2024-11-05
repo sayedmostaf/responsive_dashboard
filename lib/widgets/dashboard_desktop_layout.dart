4c32015c0e3a60502e6c8a1a3e1007f4fc825425
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-11-05 20:47:07 +0200

    refactor code

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
-import 'package:responsive_dashboard/widgets/dots_indicator.dart';
-import 'package:responsive_dashboard/widgets/my_card.dart';
-import 'package:responsive_dashboard/widgets/my_cards_page_view.dart';
 import 'package:responsive_dashboard/widgets/my_cards_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
