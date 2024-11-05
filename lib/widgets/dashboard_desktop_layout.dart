b2cfc95af1b8bd4f8e1761b8e301e10e58c549ca
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-11-05 20:09:30 +0200

    create dots indicator row

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/widgets/dots_indicator.dart';
 import 'package:responsive_dashboard/widgets/my_card.dart';
 import 'package:responsive_dashboard/widgets/my_cards_page_view.dart';
 
@@ -22,9 +23,10 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(
-          child: MyCardsPageView(),
-        ),
+        // Expanded(
+        //   child: MyCardsPageView(),
+        // ),
+        DotsIndicator(),
       ],
     );
   }
