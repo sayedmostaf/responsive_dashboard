abb661dfded45b188118bdce8631c12c61916f8a
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-11-07 12:04:40 +0200

    income details

---
+++ lib/widgets/income_details.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/item_details_model.dart';
+import 'package:responsive_dashboard/widgets/item_details.dart';
+
+class IncomeDetails extends StatelessWidget {
+  const IncomeDetails({super.key});
+  static const items = [
+    ItemDetailsModel(
+      color: Color(0xFF208BC7),
+      title: 'Design service',
+      value: '%40',
+    ),
+    ItemDetailsModel(
+      color: Color(0xFF4DB7F2),
+      title: 'Design product',
+      value: '%25',
+    ),
+    ItemDetailsModel(
+      color: Color(0xFF064060),
+      title: 'Product royalti',
+      value: '%20',
+    ),
+    ItemDetailsModel(
+      color: Color(0xFFE2DECD),
+      title: 'Other',
+      value: '%22',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      shrinkWrap: true,
+      itemBuilder: (context, index) => ItemDetails(
+        itemDetailsModel: items[index],
+      ),
+    );
+  }
+}
--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/custom_background_container.dart';
 import 'package:responsive_dashboard/widgets/income_chart.dart';
+import 'package:responsive_dashboard/widgets/income_details.dart';
 import 'package:responsive_dashboard/widgets/income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -14,9 +15,13 @@ class IncomeSection extends StatelessWidget {
           IncomeSectionHeader(),
           Expanded(
               child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: IncomeChart(),
+              ),
+              Expanded(
+                child: IncomeDetails(),
               ),
             ],
           ))
