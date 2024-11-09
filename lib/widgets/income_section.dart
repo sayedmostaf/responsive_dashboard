4aa3a2af2c7ac875e04777da8bab5a812d076bda
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-11-09 21:35:20 +0200

    responsive income section

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/custom_background_container.dart';
 import 'package:responsive_dashboard/widgets/income_chart.dart';
 import 'package:responsive_dashboard/widgets/income_details.dart';
+import 'package:responsive_dashboard/widgets/income_section_body.dart';
 import 'package:responsive_dashboard/widgets/income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -13,17 +14,7 @@ class IncomeSection extends StatelessWidget {
       child: Column(
         children: [
           IncomeSectionHeader(),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: IncomeChart(),
-              ),
-              Expanded(
-                child: IncomeDetails(),
-              ),
-            ],
-          )
+          IncomeSectionBody(),
         ],
       ),
     );
---
+++ lib/widgets/income_section_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/income_chart.dart';
+import 'package:responsive_dashboard/widgets/income_details.dart';
+
+class IncomeSectionBody extends StatelessWidget {
+  const IncomeSectionBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return width >= 1200 && width < 1750
+        ? const SizedBox()
+        : const Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: IncomeChart(),
+              ),
+              Expanded(
+                flex: 2,
+                child: IncomeDetails(),
+              ),
+            ],
+          );
+  }
+}
