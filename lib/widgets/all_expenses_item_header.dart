2c0dee55781aa08f330854fdb2d833d531881155
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-11-18 13:17:59 +0200

    resposnvie mobile layout part 2

--- lib/widgets/all_expenses_item_header.dart
+++ lib/widgets/all_expenses_item_header.dart
@@ -10,19 +10,23 @@ class AllExpensesItemHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          width: 60,
-          height: 60,
-          padding: const EdgeInsets.all(14),
-          decoration: ShapeDecoration(
-            shape: const OvalBorder(),
-            color: imageBackground ?? const Color(0xfffafafa),
-          ),
-          child: SvgPicture.asset(
-            image,
-            colorFilter: ColorFilter.mode(
-              imageColor ?? const Color(0xff4eb7f2),
-              BlendMode.srcIn,
+        Flexible(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: ShapeDecoration(
+                shape: const OvalBorder(),
+                color: imageBackground ?? const Color(0xfffafafa),
+              ),
+              child: Center(
+                child: SvgPicture.asset(
+                  image,
+                  colorFilter: ColorFilter.mode(
+                    imageColor ?? const Color(0xff4eb7f2),
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
             ),
           ),
         ),
