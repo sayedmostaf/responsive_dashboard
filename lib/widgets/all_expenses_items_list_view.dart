a4fb77501f5a2e8220b3108aeb247c341073cee5
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-11-18 13:13:01 +0200

    resposnive layout part 1

--- lib/widgets/all_expenses_items_list_view.dart
+++ lib/widgets/all_expenses_items_list_view.dart
@@ -37,30 +37,20 @@ class _AllExpensesItemsListViewState extends State<AllExpensesItemsListView> {
       children: items.asMap().entries.map((e) {
         int index = e.key;
         var item = e.value;
-        if (index == 1) {
-          return Expanded(
-            child: GestureDetector(
-              onTap: () => updateIndex(index),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: AllExpensesItem(
-                  itemModel: item,
-                  isSelected: selectedIndex == index,
-                ),
-              ),
-            ),
-          );
-        } else {
-          return Expanded(
-            child: GestureDetector(
-              onTap: () => updateIndex(index),
+        return Expanded(
+          child: GestureDetector(
+            onTap: () {
+              updateIndex(index);
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: index == 1 ? 12 : 0),
               child: AllExpensesItem(
-                itemModel: item,
                 isSelected: selectedIndex == index,
+                itemModel: item,
               ),
             ),
-          );
-        }
+          ),
+        );
       }).toList(),
     );
   }
